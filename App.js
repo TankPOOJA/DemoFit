a5c2bc55d527186dee3d3a1c3870a364ac6275ab
TankPOOJA <poojatank299@gmail.com>
2021-05-19 17:15:02 +0530

    first project

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View ,StatusBar} from 'react-native';
+import {Login} from './screens/login/Login';
+import {Welcome} from './screens/welcome/Welcome'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen  options={{headerShown: false}}  name="Welcome" component={Welcome} />
+      <Stack.Screen  options={{headerShown: false}}  name="Login" component={Login} />
+
+    </Stack.Navigator>
+  </NavigationContainer>
+
+  );
+}
+
+export default App;
---
+++ screens/splash/Splash.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Image, StatusBar, StyleSheet, Text} from 'react-native';
+
+export const Splash = () => {
+  return (
+    <View style={styles.SplashScreen_RootView}>
+      <StatusBar
+        hidden={true}
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <Text style={styles.SplashScreen_ChildView}>Fitter, Healthier, Happier</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  SplashScreen_RootView: {
+    justifyContent: 'center',
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+  },
+
+  SplashScreen_ChildView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    color:"#203354",
+    fontSize:25,
+    alignContent:'center',
+    alignSelf:'center'
+  },
+});
